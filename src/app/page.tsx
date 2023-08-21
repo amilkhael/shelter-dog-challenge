d7e45dcff5c516580c77e6fec5435457b5486048
alucard0 <amilkhael@outlook.com>
2023-08-20 23:15:10 -0500

    feat: create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,60 @@
-export default function Home() {
-  return <main>home</main>
+"use client"
+import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+
+export default function LogIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get("email"),
+      name: data.get("name"),
+    })
+  }
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component='h1' variant='h5'>
+          Log in
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='name'
+            label='Name'
+            type='text'
+            id='name'
+            autoComplete='name'
+            autoFocus
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+          />
+
+          <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+            Log In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
